0fa89b10b21553627d4a2166d73b3a687c4ab523
Matthew Weier O'Phinney <matthew@zend.com>
2018-03-06 14:24:16 -0600

    Ensure router passes integration tests for implicit HEAD/OPTIONS requests
    Updates to use the latest (proposed) tests from zend-expressive-router
    for testing router capabilities surrounding implicit HEAD and OPTIONS
    requests, as well as correctly identifying allowed methods.
    
    To pass the tests, one change was reverted from #37: we no longer check
    in the router for a HEAD request and re-check against GET.

--- src/ZendRouter.php
+++ src/ZendRouter.php
@@ -96,11 +96,6 @@ class ZendRouter implements RouterInterface
 
         $zendRequest = Psr7ServerRequest::toZend($request, true);
         $match = $this->zendRouter->match($zendRequest);
-        if ($request->getMethod() === RequestMethod::METHOD_HEAD
-            && (null === $match || null !== $match->getParam(self::METHOD_NOT_ALLOWED_ROUTE))
-        ) {
-            $match = $this->zendRouter->match($zendRequest->setMethod(RequestMethod::METHOD_GET));
-        }
 
         if (null === $match) {
             // No route matched at all; to indicate that it's not due to the
